f5bb094098473263c6b85d00cb744bc5262caff5
инкогнито <vileflesh@2ch.hk>
2018-07-31 22:37:26 +0300

    Photo Pages Design

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
 	belongs_to :user
 
-	has_attached_file :image, style: { medium: "300x300>", thumb: "100x100>" }, validate_media_type: false
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, validate_media_type: false
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
